a3b3ec11efa8092ac53e208bf3310d81efcf0a07
Jang SeokBin <feel00k0810@gmail.com>
2024-01-11 15:03:20 +0900

    Squashed commit of the following:
    commit 941df919f84fe00bdabb28020aa1c65b07cca2b4
    Author: Jang SeokBin <feel00k0810@gmail.com>
    Date:   Thu Jan 11 14:47:07 2024 +0900
    
        findGradebyIdAndName
    
        Add method

--- src/model/MemoryRepository.java
+++ src/model/MemoryRepository.java
@@ -28,12 +28,22 @@ public class MemoryRepository {
     }
 
 
+
     //전체 수강생 목록 조회
     public List<Student> findAllStudent() {
         return studentList;
     }
-}
 
+    // 학생 등급 조회
+    public List<Score> findGradebyIdAndName(int studentId, String subjectName) {
+        List<Score> gradeList = this.scoreList;
+        for (Score score : scoreList) {
+            if (score.getStudent().getStudentId() == studentId && score.getSubject().getSubjectName().equals(subjectName)) {
+                gradeList.add(score);
+            }
+        }
+        return gradeList;
+    }
 
     //학생 등록
     public void addMember(String name, List<Subject> subjectList) {
