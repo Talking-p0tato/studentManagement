2e890393821d7e814ea51870f0464b83e4de67bd
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-01-11 16:28:40 +0900

    feat : addTestScore추가
    수강생의 과목별 시험 회차 및 점수를 등록할 수 있습니다

--- src/model/MemoryRepository.java
+++ src/model/MemoryRepository.java
@@ -17,11 +17,12 @@ public class MemoryRepository {
 
     private MemoryRepository() {
     }
-    
-    public void addTestScore(int studentId, String subjectName, int round, int score) {
-        
-    }
 
+    public void addTestScore(Student student, Subject subject, int round, int score) {
+        Score newscore = new Score(student, subject, round, score);
+        newscore.setGrade();
+        scoreList.add(newscore);
+    }
 
     public Optional<Student> findStudentById(int studentId) {
         return studentList.stream()
@@ -29,14 +30,21 @@ public class MemoryRepository {
                 .findFirst();
     }
 
-    public boolean isStudentExists(int studentId) {
-        return findStudentById(studentId).isEmpty();
+    //true : id를 가진 학생 있음, false : id를 가진 학생 없음
+    public boolean isStudentExist(int studentId) {
+        return !findStudentById(studentId).isEmpty();
     }
 
-    public Optional<Score> findScoreRecord(int studentId, String subjectName) {
+    public Optional<Score> findScoreRecord(int studentId, String subjectName, int round) {
         return scoreList.stream()
                 .filter(score -> score.getStudent().getStudentId() == studentId)
                 .filter(score -> score.getSubject().getSubjectName().equals(subjectName))
+                .filter(score -> score.getRound() == round)
                 .findFirst();
+    }
+
+    //true : 해당 과목에 차수에 대한 점수 존재, false : 해당 과목 차수에 대한 점수 없음
+    public boolean isScoreRecordExist(int studentId, String subjectName, int round) {
+        return !findScoreRecord(studentId, subjectName, round).isEmpty();
     }
 }
