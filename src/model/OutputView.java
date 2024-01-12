5ea67eb32f48796f06d455cd939cb480b26852ba
kiwimel0n <alstkd8580@naver.com>
2024-01-12 11:26:56 +0900

    "입력받은 시험 회차별 등급 출력문 메서드 등록"

--- src/model/OutputView.java
+++ src/model/OutputView.java
@@ -1,7 +1,8 @@
 package model;
 
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
 
 public class OutputView {
 
@@ -20,7 +21,7 @@ public class OutputView {
         System.out.println("1. (과목별) 시험 회차 및 점수 등록");
         System.out.println("2. (과목별) 시험 회차별 등급 조회 (수강생의 특정 과목 시험 회차별 등급을 조회할 수 있습니다.)");
         System.out.println("3. (과목별) 시험 회차 점수 수정");
-        System.out.println("0. 메인 메뉴로 이동"+"\n"+"\n");
+        System.out.println("0. 메인 메뉴로 이동"+"\n");
         System.out.print("원하시는 작업 메뉴 번호를 입력해주세요 : ");
         //상세메뉴번호 입력 인풋
         // 잘못된 번호 또는 문자 입력시 System.out.println("잘못된 입력입니다. 다시입력해주세요.") -> 상세메뉴 입력화면
@@ -37,12 +38,25 @@ public class OutputView {
     }
 
     //4-2 (과목별) 시험 회차별 등급 조회 입력 받는 출력문
-    public void showQuerySubjectRoundGradeScreenInput(){
+    public void showQuerySubjectRoundGradeInputScreen(){
         System.out.println("|]----수강생 과목 회차별 등급 조회----[|"+"\n");
         System.out.println("조회할 과목을 입력해주세요.");
         System.out.println("-> ");
         //과목 이름 or 과목 고유번호 입력
 
+    }
+    public void showQuerySubjectRoundGradeScreenOutput(String subject, List<Score> scoreList){
+        //입력받은 시험 회차별 등급 출력
+        List<Score> filteredList = scoreList.stream().filter(score ->subject
+                .equals(score.getSubject().getSubjectName()))
+                .map(score -> new Score(score.getStudent(), score.getSubject(), score.getRound(), score.getScore()))
+                .collect(Collectors.toList());
+        for(int i = 0; i < filteredList.size(); i++){
+            Score studentScore = filteredList.get(i);
+            System.out.printf("%d. %d회차 : %s"+"\n", i+1, studentScore.getRound(),studentScore.getGrade());
+        }
+        System.out.println("0. 돌아가기");
+        System.out.println("->");
     }
 
 
@@ -70,6 +84,9 @@ public class OutputView {
         // 수정 회차 선택화면
         System.out.println("수정을 원하는 회차를 입력 해주세요.");
         System.out.println("과목명 : "+ subject);
+        //ex) 1. 1회차 : 90
+        //    2. 3회차 : 85
+        //    3. 5회차 : 70
         for(int i = 0; i < scoreList.size(); i++){
             Score studentScore = scoreList.get(i);
             System.out.printf("%d. %d회차 : %d"+"\n" ,i+1, studentScore.getRound(), studentScore.getScore());
