179e3ab0cdfe89ed52a07a7973619bd8577a4e37
kiwimel0n <alstkd8580@naver.com>
2024-01-12 18:58:28 +0900

    "4-1 학생 점수등록 로직 메서드"

--- src/model/StudentManagement.java
+++ src/model/StudentManagement.java
@@ -7,7 +7,13 @@ import java.util.List;
 
 
 public class StudentManagement {
+    public  int studentId;
+    public  int menuSelect;
 
+    public  String subjectName;
+
+    public  int round;
+    public int score;
     OutputView outputView= new OutputView();
     Scanner scanner = new Scanner(System.in);
 
@@ -30,21 +36,68 @@ public class StudentManagement {
                 //메인메뉴로 진입하는 메서드
                 break;
             case 1:
-
-                break;
+                AddStudentScoreProcess(studentId);
+            break;
             case 2:
                 //과목별 회차 등급 조회 메서드
                 break;
             case 3:
                 //과목별 회차 점수 수정 메서드
                 break;
-
             default :
                 outputView.showWrongAddContext();
                 selectStudentManageInfo(studentId);
-
         }
     }
+
+    //4-1 페이지 작동 로직
+    public void AddStudentScoreProcess(int studentId) {
+        outputView.showAddSubjectRoundScoreFrontScreen();
+        //학생이 수강한 과목정보를 출력하는 메서드
+        AddSubjectNameProcess(studentId);
+    }
+
+    public void AddSubjectNameProcess(int studentId){
+        outputView.showAddSubjectNameScreen();
+        subjectName = scanner.next();
+
+        if (MemoryRepository.getInstance().isValidSubject(studentId, subjectName)){
+            AddRondProcess(studentId,subjectName);
+        }else {
+            outputView.showWrongAddContext();
+            AddSubjectNameProcess(studentId);
+        }
+
+    }
+
+    public void AddRondProcess(int studentId, String subjectName){
+        outputView.showAddRoundScreen();
+        round = scanner.nextInt();
+        if (MemoryRepository.getInstance().isScoreRecordExist(studentId, subjectName, round)){
+            AddScoreProcess(studentId, subjectName, round);
+        }else {
+            outputView.showWrongAddContext();
+            AddRondProcess(studentId, subjectName);
+        }
+    }
+
+    public void AddScoreProcess(int studentId,String subjectName, int round){
+        outputView.showAddScoreScreen();
+        int score = scanner.nextInt();
+        if (MemoryRepository.getInstance().validateAndParseScore(score)) {
+            MemoryRepository.getInstance().addTestScore(
+                    MemoryRepository.getInstance().getStudentList().get(studentId),
+                    MemoryRepository.getInstance().getSubjectByName(subjectName),
+                    round,
+                    score);
+            outputView.showConfirmAddStudentScore();
+            selectStudentManageInfo(studentId);
+        } else {
+            outputView.showWrongAddContext();
+                AddScoreProcess(studentId, subjectName, round);
+        }
+    }
+    // ------- 4 - 1여기까지 작동로직-----
 
     public void showStudentInfo() {
         OutputView.showStudentList();
@@ -53,11 +106,11 @@ public class StudentManagement {
 
         switch (input) {
             case 1:
-                if (repository.getStudentList().isEmpty()) {
+                if (MemoryRepository.getInstance().getStudentList().isEmpty()) {
                     System.out.println("조회 할 수 있는 수강생이 없습니다.");
                 } else {
-                    System.out.println("등록된 수강생은 " + repository.getStudentList() + "명 입니다.");
-                    for (Student value : repository.getStudentList()) {
+                    System.out.println("등록된 수강생은 " + MemoryRepository.getInstance().getStudentList() + "명 입니다.");
+                    for (Student value : MemoryRepository.getInstance().getStudentList()) {
                         System.out.println(" 수강생ID : " + value.getStudentId() + "\n수강생이름 : " + value.getMemberName() + "'\n수강신청한과목 : " + value.getSubjectList().toString());
                     }
                 }
