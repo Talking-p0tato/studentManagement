c3b752e7c227835a1b0db0a573fc2ab3dcc7f7c7
kiwimel0n <alstkd8580@naver.com>
2024-01-12 16:49:02 +0900

    "수강생 점수관리 플로우 메서드"

--- src/model/StudentManagement.java
+++ src/model/StudentManagement.java
@@ -1,7 +1,46 @@
 package model;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 public class StudentManagement {
 
-    MemoryRepository repository = MemoryRepository.getInstance();
+    OutputView outputView= new OutputView();
+    Scanner scanner = new Scanner(System.in);
+
+    public void processStudentManage() {
+        outputView.showAddStudentIdScreen();
+        studentId = scanner.nextInt();
+        if (MemoryRepository.getInstance().isStudentExist(studentId)) {
+            selectStudentManageInfo(studentId);
+        }else {
+            System.out.println("해당 ID를 가진 학생이 없습니다. 다시 입력해주세요.");
+            processStudentManage();
+        }
+    }
+
+    private void selectStudentManageInfo(int studentId) {
+        outputView.showStudentScoreManageInfo();
+        menuSelect = scanner.nextInt();
+        switch (menuSelect) {
+            case 0:
+                //메인메뉴로 진입하는 메서드
+                break;
+            case 1:
+
+                break;
+            case 2:
+                //과목별 회차 등급 조회 메서드
+                break;
+            case 3:
+                //과목별 회차 점수 수정 메서드
+                break;
+
+            default :
+                outputView.showWrongAddContext();
+                selectStudentManageInfo(studentId);
+
+        }
+    }
 
 }
