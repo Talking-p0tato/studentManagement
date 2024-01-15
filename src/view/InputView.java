a2cefee9fd05c6da065e58c65b08789bae031233
Jang SeokBin <feel00k0810@gmail.com>
2024-01-15 15:51:45 +0900

    Bug fix : addStudent method

--- src/view/InputView.java
+++ src/view/InputView.java
@@ -9,23 +9,34 @@ public class InputView {
     static Scanner scanner = new Scanner(System.in);
 
     public static int getUserIntInput() {
-        System.out.println("번호를 입력해주세요");
+        System.out.print("번호를 입력해주세요. : ");
         return scanner.nextInt();
     }
 
     public static int getRoundByUser() {
-        System.out.println("등록할 회차를 입력해주세요.");
+        System.out.print("등록할 회차를 입력해주세요. : ");
         return scanner.nextInt();
     }
 
     public static int getScoreByUser() {
-        System.out.println("등록할 점수를 입력해 주세요.");
+        System.out.print("등록할 점수를 입력해 주세요. : ");
         return scanner.nextInt();
     }
 
     public static String getUserStringInput() {
-        System.out.println("입력해주세요");
+        System.out.print("입력해주세요. : ");
         return scanner.next();
+    }
+
+    public static String getUserStringInputNoMessageNext() {
+        String input = scanner.next();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static String getUserStringInputNoMessageNextline() {
+        String input = scanner.nextLine();
+        return input;
     }
 
     public static int getUserIntInputNoMessage() {
