723e01eeb6266b422aab567a5c7a3c3469123e19
TTPDE <bumjin0314@gmail.com>
2024-01-15 17:10:03 +0900

    aaaa

--- src/controller/StudentManagement.java
+++ src/controller/StudentManagement.java
@@ -344,7 +344,7 @@ public class StudentManagement {
         // 회차 및 점수 출력
         OutputView.showUpdateStudentRoundGrade(repository.findSubjectNameById(subjectId), scores);
 
-        // 회차 선택.
+        // 회차 선택.cl
         int roundNumber;
         while (true) {
             roundNumber = InputView.getUserIntInput();
